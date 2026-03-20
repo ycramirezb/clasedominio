3488b317b1e22f556a9f586d2a4de093da294b6b
ycramirezb <yeferson1999ramirez@gmail>
2026-03-20 08:13:00 -0500

    se agrergo el estilo.css

--- src/main/java/com/Ejercicio/Ejercicio/web/SecurityConfig.java
+++ src/main/java/com/Ejercicio/Ejercicio/web/SecurityConfig.java
@@ -40,6 +40,7 @@ public UserDetailsService users(PasswordEncoder passwordEncoder){
 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
     http
     .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/css/**", "/js/**", "/images/**", "/webjars/**").permitAll()
         .requestMatchers("/","/detalle").authenticated()
         .requestMatchers("/nuevo", "/guardar", "/editar", "/eliminar", "/guardarDemo")
         .hasRole("ADMIN")
